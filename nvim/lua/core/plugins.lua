d59c670545fe4d126c52ba0e2e8f9af6c6f37894
Nikola Kovacevic <nikola.kovacevic@igt.com>
2023-12-22 22:05:50 +0100

    reformat code and remove auto recognition of tabs

--- nvim/lua/core/plugins.lua
+++ nvim/lua/core/plugins.lua
@@ -1,104 +1,103 @@
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system {
-		'git',
-		'clone',
-		'--filter=blob:none',
-		'https://github.com/folke/lazy.nvim.git',
-		'--branch=stable',
-		lazypath,
-	}
+    vim.fn.system {
+        'git',
+        'clone',
+        '--filter=blob:none',
+        'https://github.com/folke/lazy.nvim.git',
+        '--branch=stable',
+        lazypath,
+    }
 end
 vim.opt.rtp:prepend(lazypath)
 
 
 require('lazy').setup({
-	'tpope/vim-fugitive',
-	'tpope/vim-sleuth', -- Auto adjust shiftwidth and expandtab
+    'tpope/vim-fugitive',
 
-	{
-		'hrsh7th/nvim-cmp',
-		dependencies = {
-			'hrsh7th/cmp-nvim-lsp',
-			'hrsh7th/cmp-path',
-			'hrsh7th/cmp-buffer',
-			'L3MON4D3/LuaSnip',
-		}
-	},
+    {
+        'hrsh7th/nvim-cmp',
+        dependencies = {
+            'hrsh7th/cmp-nvim-lsp',
+            'hrsh7th/cmp-path',
+            'hrsh7th/cmp-buffer',
+            'L3MON4D3/LuaSnip',
+        }
+    },
 
 
     {
-		'folke/neodev.nvim',
-		opts = {}
-	},
+        'folke/neodev.nvim',
+        opts = {}
+    },
 
-	{
-		'neovim/nvim-lspconfig',
-		dependencies = {
-			'williamboman/mason.nvim',
-			'williamboman/mason-lspconfig.nvim',
-			'j-hui/fidget.nvim',
-			'folke/neodev.nvim',
-		},
-	},
+    {
+        'neovim/nvim-lspconfig',
+        dependencies = {
+            'williamboman/mason.nvim',
+            'williamboman/mason-lspconfig.nvim',
+            'j-hui/fidget.nvim',
+            'folke/neodev.nvim',
+        },
+    },
 
-	{
-		'EdenEast/nightfox.nvim',
-		priority = 1000,
-		config = function()
-			vim.cmd.colorscheme 'carbonfox'
-		end,
-	},
+    {
+        'EdenEast/nightfox.nvim',
+        priority = 1000,
+        config = function()
+            vim.cmd.colorscheme 'carbonfox'
+        end,
+    },
 
-	{
-		'nanozuki/tabby.nvim',
-		opts = {},
-	},
+    {
+        'nanozuki/tabby.nvim',
+        opts = {},
+    },
 
-	{
-		'nvim-lualine/lualine.nvim',
-		dependencies = { 'nvim-tree/nvim-web-devicons' },
-	},
+    {
+        'nvim-lualine/lualine.nvim',
+        dependencies = { 'nvim-tree/nvim-web-devicons' },
+    },
 
-	{
-		'nvim-telescope/telescope.nvim',
-		tag = '0.1.3',
-		dependencies = { 'nvim-lua/plenary.nvim' },
-		opts = {},
-	},
+    {
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.3',
+        dependencies = { 'nvim-lua/plenary.nvim' },
+        opts = {},
+    },
 
-	{
-		'nvimdev/dashboard-nvim',
-		event = 'VimEnter',
-		dependencies = { 'nvim-tree/nvim-web-devicons' }
-	},
+    {
+        'nvimdev/dashboard-nvim',
+        event = 'VimEnter',
+        dependencies = { 'nvim-tree/nvim-web-devicons' }
+    },
 
-	{
-		"lukas-reineke/indent-blankline.nvim",
-		main = "ibl",
-	},
+    {
+        "lukas-reineke/indent-blankline.nvim",
+        main = "ibl",
+    },
 
-	{
-		'nvim-treesitter/nvim-treesitter',
-		opts = {}
-	},
+    {
+        'nvim-treesitter/nvim-treesitter',
+        opts = {}
+    },
 
-	{
-		'nvim-tree/nvim-tree.lua',
-		lazy = false,
-		dependencies = {
-			"nvim-tree/nvim-web-devicons",
-		},
-	},
+    {
+        'nvim-tree/nvim-tree.lua',
+        lazy = false,
+        dependencies = {
+            "nvim-tree/nvim-web-devicons",
+        },
+    },
 
-	{
-		"LintaoAmons/scratch.nvim",
-		event = "VeryLazy",
-	},
+    {
+        "LintaoAmons/scratch.nvim",
+        event = "VeryLazy",
+    },
 
-	{
-		"terrortylor/nvim-comment",
-	}
+    {
+        "terrortylor/nvim-comment",
+    }
 
 }, {})
