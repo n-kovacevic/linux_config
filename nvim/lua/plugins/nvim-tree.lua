3de362def538abf9f2c9e6e3eb4bfbc2b7812ba9
Nikola Kovacevic <nikola.kovacevic@igt.com>
2023-12-22 21:40:11 +0100

    changed nvim-tree arrows

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -1,3 +1,16 @@
 -- [ Configure nvim-tree ]
-require('nvim-tree').setup({})
+vim.g.loaded = 1
+vim.g.loaded_netrwPlugin = 1
+require('nvim-tree').setup({
+	renderer = {
+		icons = {
+			glyphs = {
+				folder = {
+					arrow_closed = "󰨃",
+					arrow_open = "󰚶",
+				}
+			}
+		}
+	}
 
+})
