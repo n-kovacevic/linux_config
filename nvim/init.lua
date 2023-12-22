4f68fbb95c26dde70a376f987393685c337831cd
Nikola Kovacevic <nikola.kovacevic@igt.com>
2023-12-22 20:26:57 +0100

    fix neodev

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,11 +1,11 @@
 require("core/plugins")
 require("core/options")
 require("core/keymaps")
+require('neodev').setup()
 require("plugins/nvim-cmp")
 require("plugins/nvim-lsp")
 require("plugins/indent-blackline")
 require("plugins/nvim-tree")
 
-require('neodev').setup()
 
 
