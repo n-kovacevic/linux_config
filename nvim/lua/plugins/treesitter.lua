8165c7aca2f9f657da63d1593bdd24cdfc15e37d
Nikola Kovacevic <nikola.kovacevic@igt.com>
2023-12-26 15:30:28 +0100

    add bash vim and vimdoc to treesitter

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ require('nvim-treesitter.configs').setup({
 
     indent = { enable = true },
     auto_install = false,
-    ensure_installed = {'bash'},
+    ensure_installed = {'bash', 'vimdoc', 'vim'},
     sync_install = false,
     ignore_install = {}
 })
