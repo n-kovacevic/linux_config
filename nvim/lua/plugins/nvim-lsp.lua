8c092af04ac5ee0fe31b468a48f63aa2cf6b153e
Nikola Kovacevic <nikola.kovacevic@igt.com>
2023-12-27 18:44:03 +0100

    lsp config changed

--- nvim/lua/plugins/nvim-lsp.lua
+++ nvim/lua/plugins/nvim-lsp.lua
@@ -12,12 +12,17 @@ require('mason').setup({
 require('mason-lspconfig').setup()
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 local lspconfig = require 'lspconfig'
+lspconfig.ansiblels.setup({ capabilities = capabilities })
 lspconfig.bashls.setup({ capatibilities = capabilities })
-lspconfig.lua_ls.setup({ capabilities = capabilities })
-lspconfig.groovyls.setup({ capabilities = capabilities })
+-- lspconfig.biome.setup({ capabilities = capabilities })
+lspconfig.docker_compose_language_service.setup({ capatibilities = capabilities })
+lspconfig.dockerls.setup({ capatibilities = capabilities })
+-- lspconfig.helm_ls.setup({ capatibilities = capabilities })
+-- lspconfig.html.setup({ capatibilities = capabilities })
+-- lspconfig.jdtls.setup({ capabilities = capabilities })
+lspconfig.jsonls.setup({ capatibilities = capabilities })
 lspconfig.lemminx.setup({ capabilities = capabilities })
-lspconfig.jsonls.setup({ capabilities = capabilities })
-lspconfig.biome.setup({ capabilities = capabilities })
+lspconfig.lua_ls.setup({ capabilities = capabilities })
 lspconfig.pylsp.setup({
 	capabilities = capabilities,
 	settings = {
@@ -30,4 +35,3 @@ lspconfig.pylsp.setup({
 		}
 	}
 })
-
