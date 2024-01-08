a7683c00e533796eada08e6a6851be2e604690e1
Nikola Kovacevic <nikola.kovacevic@igt.com>
2024-01-08 12:38:36 +0100

    added ScratchPad

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -9,6 +9,7 @@ vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
 -- Add scratch keybindings
 vim.keymap.set("n", "<M-C-n>", "<cmd>Scratch<cr>")
 vim.keymap.set("n", "<M-C-o>", "<cmd>ScratchOpen<cr>")
+vim.keymap.set("n", "<M-C-p>", "<cmd>ScratchPad<cr>")
 
 -- Disable delete moving data to register
 vim.keymap.set('n', 'x', '"_x')
--- nvim/lua/plugins/dashboard.lua
+++ nvim/lua/plugins/dashboard.lua
@@ -7,6 +7,7 @@ require('dashboard').setup {
 		shortcut = {
             { desc = ' New file', group = 'RainbowGreen', action = 'Scratch', key = 'n', },
             { desc = ' Open file', group = 'RainbowCyan', action = 'ScratchOpen', key = 'o', },
+            { desc = ' ScratchPad', group = 'RainbowYellow', action = 'ScratchPad', key = 'p', },
 			{ desc = '󰊳 Update', group = 'RainbowViolet', action = 'Lazy update', key = 'u' },
 			{ desc = ' Files', group = 'RainbowOrange', action = 'Telescope find_files', key = 'f', },
 		}
