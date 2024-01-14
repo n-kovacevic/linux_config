0ec2d5c48bd430d3e3c3cd13da398fe5c20025b6
Nikola Kovacevic <contact@nkovacevic.com>
2024-01-14 17:34:32 +0100

    reduce esc time in tmux, setup keybind for nvim-cmp

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -23,6 +23,7 @@ cmp.setup {
 		['<C-b>'] = cmp.mapping.scroll_docs(-4),
 		['<C-f>'] = cmp.mapping.scroll_docs(4),
 		['<C-e>'] = cmp.mapping.abort(),
+		['<Esc>'] = cmp.mapping.abort(),
 		['<C-Space>'] = cmp.mapping.complete(),
 		['<CR>'] = cmp.mapping.confirm(),
 		['<Tab>'] = cmp.mapping.confirm(),
