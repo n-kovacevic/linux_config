e505224a307598a4b96e936ba74736af8dea2290
Nikola Kovacevic <nikola.kovacevic@igt.com>
2024-01-18 23:18:28 +0100

    nvim-tree updates

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -1,17 +1,24 @@
 -- [ Configure nvim-tree ]
 vim.g.loaded = 1
 vim.g.loaded_netrwPlugin = 1
-require('nvim-tree').setup({
+require("nvim-tree").setup({
 	renderer = {
 		icons = {
 			glyphs = {
 				folder = {
 					arrow_closed = "󰨃",
 					arrow_open = "󰚶",
-				}
-			}
+				},
+			},
+			git_placement = "signcolumn",
 		},
-        group_empty = true
-    }
-
+        highlight_git = true,
+        highlight_opened_files = "icon",
+		group_empty = true,
+		indent_markers = {
+			enable = true,
+		},
+	},
+	filters = { custom = { "^.git$" } },
 })
+-- vim.api.nvim_command('hi NvimTreeNormal guibg=none')
