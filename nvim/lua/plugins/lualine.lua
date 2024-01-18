2bdf09190ec6ad118296323157855c4af819699b
Nikola Kovacevic <nikola.kovacevic@igt.com>
2024-01-18 23:18:04 +0100

    lualine - updated theme

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,20 +1,22 @@
-local lualine = require('lualine')
-
-lualine.setup {
+local lualine = require("lualine")
+local theme = require("lualine.themes.auto")
+theme.normal.c.bg = 'none'
+lualine.setup({
 	options = {
-		component_separators = '|',
-		section_separators = { left = '', right = '' },
+        theme = theme,
+		component_separators = "|",
+		section_separators = { left = "", right = "" },
 		disabled_filetypes = {
-			statusline = {'NvimTree'},
-			winbar = {}
-		}
+			-- statusline = {'NvimTree'},
+			-- winbar = {'NvimTree'}
+		},
 	},
 	sections = {
 		lualine_a = {
-			{ 'mode', separator = { left = '' }, right_padding = 2 },
+			{ "mode", separator = { left = "" }, right_padding = 2 },
 		},
 		lualine_z = {
-			{ 'location', separator = { right = '' }, left_padding = 2 },
+			{ "location", separator = { right = "" }, left_padding = 2 },
 		},
-	}
-}
+	},
+})
