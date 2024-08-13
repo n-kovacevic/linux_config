57aaa82ddb59f3f7f50feeff417fe1ab9c18c1fe
Nikola Kovacevic <nikola.kovacevic@igt.com>
2024-08-13 19:56:05 +0200

    remove scratchpad from dashboard

--- nvim/lua/nkovacevic/plugins/dashboard.lua
+++ nvim/lua/nkovacevic/plugins/dashboard.lua
@@ -11,7 +11,7 @@ return {
             shortcut = {
                 { desc = ' New file', group = 'RainbowGreen', action = 'Scratch', key = 'n', },
                 { desc = ' Open file', group = 'RainbowCyan', action = 'ScratchOpen', key = 'o', },
-                { desc = ' ScratchPad', group = 'RainbowYellow', action = 'ScratchPad', key = 'p', },
+                -- { desc = ' ScratchPad', group = 'RainbowYellow', action = 'ScratchPad', key = 'p', },
                 { desc = '󰊳 Update', group = 'RainbowViolet', action = 'Lazy update', key = 'u' },
                 { desc = ' Files', group = 'RainbowOrange', action = 'Telescope find_files', key = 'f', },
             }
